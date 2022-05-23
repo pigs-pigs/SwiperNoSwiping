b067f79c915fbcdbde28071d8593b5e529df3c3f
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-23 18:43:39 -0400

    interaction for discover

--- Js/discover.js
+++ Js/discover.js
@@ -1,2 +1,25 @@
 // Search/Discover/Recommended
 
+function moveCards(direction) {
+    if (direction == "LEFT") {
+console.log("Moving Left")
+    } else if (direction == "RIGHT") {
+
+    }
+}
+
+$(document).keydown(function (e) {
+    if (e.keydown == 37) { //Left
+        moveCards("LEFT")
+    } else if (e.keydown == 39) { //Right
+        moveCards("RIGHT")
+    }
+});
+
+$("fa-angle-right").click(function () {
+    moveCards("RIGHT")
+})
+
+$("fa-angle-left").click(function () {
+    moveCards("LEFT")
+})
