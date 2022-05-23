70e8d7dc6c82ea2e6290bfd8842c6465f36c6cbe
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-23 18:47:28 -0400

    arrows

--- Js/discover.js
+++ Js/discover.js
@@ -2,16 +2,16 @@
 
 function moveCards(direction) {
     if (direction == "LEFT") {
-console.log("Moving Left")
+        console.log("Moving Left")
     } else if (direction == "RIGHT") {
 
     }
 }
 
 $(document).keydown(function (e) {
-    if (e.keydown == 37) { //Left
+    if (e.keyCode == 37) { //Left
         moveCards("LEFT")
-    } else if (e.keydown == 39) { //Right
+    } else if (e.keyCode == 39) { //Right
         moveCards("RIGHT")
     }
 });
