ea45f0b6fcbe67a74ba931d52cda07eee348a124
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-23 17:54:07 -0400

    create

---
+++ Js/create.js
@@ -0,0 +1,34 @@
+function createSet() {
+    var jsondata = {
+        "Title": "yes",
+        "Description": "long long long long longggg",
+        "CreatorId": toString(1),
+        "Cover": "https://picsum.photos/600/300",
+        "Cards": JSON.stringify({
+            Heading: "Lol",
+            Description: "yes",
+            Image: `https://picsum.photos/600/300?random=${Math.floor(Math.random() * 500)}`
+        })
+    };
+    var settings = {
+        "async": false,
+        "crossDomain": true,
+        "url": "https://swipernoswiping-3b4f.restdb.io/rest/cards",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": "60ce0b22e2c96c46a246371f",
+            "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(jsondata)
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+
+$("#submitCards").click(function () {
+    createSet()
+})
