30b126ab98a9d59429dc945fea7f4ca4f571bd61
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-31 20:03:11 -0400

    Redirects

--- Js/menu.js
+++ Js/menu.js
@@ -12,17 +12,35 @@
   
   $(document).on("click", function (e) {//TODO: unbind these in the future
     if (
-      $(".notification-btn").css("display") !== "none" &&
+      $("#notifs-container").css("display") !== "none" &&
       $(e.target).closest("#notifs-container").length === 0 &&
       $(e.target).closest(".notification-btn").length === 0
     ) {
-      $("#notifs-container").fadeToggle();
+      $("#notifs-container").fadeOut();
     }
   });
   
   $(".notification-btn").click(function () {
     $("#notifs-container").fadeToggle();
   });
+
+  function buildNotif() {
+      `<div class="message-box">
+      <img src="https://images.unsplash.com/photo-1533993192821-2cce3a8267d1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHdvbWFuJTIwbW9kZXJufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="profile image">
+      <div class="message-content">
+        <div class="message-header">
+          <div class="name">Swiper</div>
+          <i class="fa fa-star"></i>
+        </div>
+        <p class="message-line">
+          Person liked your set lolz!
+        </p>
+        <p class="message-line time">
+          1hr ago
+        </p>
+      </div>
+    </div>`
+  }
   
   $(".add-btn").click(function () {
     //TODO: redirect to create page, no topbar on that page
