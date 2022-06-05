c6f841127a7db3da77f8031aef8ce0162694b571
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-06-05 10:26:31 -0400

    Fix styles

--- Js/create.js
+++ Js/create.js
@@ -60,7 +60,7 @@ $(document).on("click", ".uploader-open", function () {
     var dialog = uploadcare.openDialog(null, null, {
         publicKey: "1ca1277bb9380dcaf55f",
         imagesOnly: true,
-        crop: "16:9,5:7",
+        crop: "16:9,5:7,9:16",//TODO:
         tabs: "file camera url gdrive gphotos"
     });
 
