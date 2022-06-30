77aa8bf3d530c775b70f559a90881406930d1080
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-06-30 15:47:37 -0400

    New Home Page

--- Js/home.js
+++ Js/home.js
@@ -1,90 +1,68 @@
-// Search/Discover/Recommended
-var FirstCard = 0
-var data
+// Setup Cards
 function createImg(ImgData) {
-    var pattern = new RegExp('^http')
-    if (pattern.test(ImgData)) {
-        return `src="${ImgData}" `
-    } else {
-        return `style="background-color: ${ImgData};" `
-    }
+  var pattern = new RegExp("^http");
+  if (pattern.test(ImgData)) {
+    return `src="${ImgData}" `;
+  } else {
+    return `style="background-color: ${ImgData};" `;
+  }
 }
 
 var settings = {
-    "async": false,
-    "crossDomain": true,
-    "url": `https://swipernoswiping-3b4f.restdb.io/rest/cards?max=150&h={"$orderby":{"_created":-1}}`,
-    "method": "GET",
-    "headers": {
-        "content-type": "application/json",
-        "x-apikey": "60ce0b22e2c96c46a246371f",
-        "cache-control": "no-cache"
-    }
-}
-$.ajax(settings).done(function (response) { data = response });
-
-function initCards(Starter) {
-    for (let index = Starter; index <= Starter + 2; index++) {
-        var Card = data[index]
-        $(".discover-cards").append(`<div data-set="${Card._id}" class="tinder--card discover--card ${index == Starter + 1 && `center-card`}">
-            <img ${createImg(Card.Cover)}>
-          <h3>${Card.Title}</h3>
-            <p>${Card.Description}</p>
-          </div>`)
-    }
-}
-
-$(".discover-cards").on("click", ".discover--card", function () {
-    if ($(this).data("set")) {
-        window.location.href = "swipe?set=" + $(this).data("set")
-    }
-})
-
-initCards(0)
-
-
-function moveCards(direction) {
-    console.log(FirstCard,data.length)
-    if (direction == "LEFT" && FirstCard > 0) {
-        FirstCard -= 1
-        $($(".discover-cards").children()[2]).remove()
-
-        var Card = data[FirstCard]
-        $(".discover-cards").prepend(`<div data-set="${Card._id}" class="tinder--card discover--card">
+  async: false,
+  crossDomain: true,
+  url: `https://swipernoswiping-3b4f.restdb.io/rest/cards?max=8&h={"$orderby":{"_created":-1}}`, //Make this be recomended/tag filtered
+  method: "GET",
+  headers: {
+    "content-type": "application/json",
+    "x-apikey": "60ce0b22e2c96c46a246371f",
+    "cache-control": "no-cache",
+  },
+};
+$.ajax(settings).done(function (response) {
+  $.each(response, function (Card) {
+    $(
+      ".discover-cards"
+    ).append(`<div data-set="${Card._id}" class="tinder--card discover--card ${index == Starter + 1 && `center-card`}">
         <img ${createImg(Card.Cover)}>
       <h3>${Card.Title}</h3>
         <p>${Card.Description}</p>
-      </div>`)
-        $($(".discover-cards").children()[2]).removeClass("center-card")
-        $($(".discover-cards").children()[1]).addClass("center-card")
-
-    } else if (direction == "RIGHT" && FirstCard < data.length - 3) {
-        FirstCard += 1
-        $($(".discover-cards").children()[0]).remove()
-        $($(".discover-cards").children()[0]).removeClass("center-card")
-        $($(".discover-cards").children()[1]).addClass("center-card")
-
-        var Card = data[FirstCard + 2]
-        $(".discover-cards").append(`<div data-set="${Card._id}" class="tinder--card discover--card">
-        <img ${createImg(Card.Cover)}>
-      <h3>${Card.Title}</h3>
-        <p>${Card.Description}</p>
-      </div>`)
-    }
-}
-
-$(document).keydown(function (e) {
-    if (e.keyCode == 37) { //Left
-        moveCards("LEFT")
-    } else if (e.keyCode == 39) { //Right
-        moveCards("RIGHT")
-    }
+      </div>`);
+  });
 });
 
-$(".fa-angle-right").click(function () {
-    moveCards("RIGHT")
-})
+$(".discover-sets").on("click", ".discover--card", function () {
+  if ($(this).data("set")) {
+    window.location.href = "swipe?set=" + $(this).data("set");
+  }
+});
 
-$(".fa-angle-left").click(function () {
-    moveCards("LEFT")
-})
+//Setup Tags
+const Tags = {
+  Trending: { Color: "linear-gradient(cyan,blue)", Icon: "arrow-trend-up" },
+  Hot: { Color: "linear-gradient(orange,red)", Icon: "fire" },
+  New: { Color: "linear-gradient(#D500FF,#7700FF)", Icon: "clock" },
+  Celebs: { Color: "goldenrod", Icon: "clapperboard" },
+  Food: { Color: "#732410", Icon: "utensils" },
+  Colors: { Color: "#1C97DA", Icon: "palette" },
+  Men: { Color: "#0B14C9", Icon: "person" },
+  Women: { Color: "#770AC9", Icon: "person-dress" },
+  // Songs: { Color: "#D5157B", Icon: "music" }
+};
+
+// Default Tag setup
+$.each(Tags, function (Name, Data) {
+  $(".tags-container").append(`
+     <div class="tag" style="background:${Data.Color};">
+        <i class="fa fa-${Data.Icon}"></i>
+        <p>${Name}</p>
+      </div>
+    `);
+});
+
+$(".tags-container").append(`
+     <div class="tag add-tag">
+        <i class="fa fa-plus"></i>
+        <p>Add Tags</p>
+      </div>
+    `);
